0d8e0696e43ebaa6293e06b36412bd8f867e3c08
Mike <mikewconroy3@gmail.com>
2021-11-04 22:12:20 -0500

    commit tree ea561758628631c43317fd6518a2b551c03a6fad

--- index.js
+++ index.js
@@ -14,14 +14,13 @@ function distanceTravelledInFeet(start, destination){
 };
 
 
-function calculatesFarePrice (start, destination) {
+function calculatesFarePrice(start, destination){
     let distance = distanceTravelledInFeet(start, destination)
-  
     if (distance <400) {
       return (0)
-    } else if (distance >400 && distance <=2000) {
+    } else if (distance <=2000) {
       return .02 * (distance - 400);
-    } else if (distance >2000 && distance <=2500) {
+    } else if (distance <=2500) {
       return (25)
     } else {
       return ('cannot travel that far')
